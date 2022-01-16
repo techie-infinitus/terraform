a98ddfa06ac2754027d59fa4660648816c681632
techie-infinitus <87640980+techie-infinitus@users.noreply.github.com>
2022-01-16 22:00:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
+#provider "aws" {
+#  region = "ap-south-1"
+#}
+
 provider "aws" {
-  region = "ap-south-1"
+  region     = "us-east-2"
+  access_key = "AKIAU7GUXHK2W77546X7"
+  secret_key = "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"
 }
